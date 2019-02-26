408013690963df20abc7429b960d1731c51fe400
201585052 <1216884492@qq.com>
2019-02-26 10:43:15 +0800

    pwa

---
+++ precache-manifest.cdd4f396efa2f875fcb6f219029c6a03.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f32ac896a4b6f6df6a19d400cca562e5",
+    "url": "static/demo3-f32ac.min.png"
+  },
+  {
+    "revision": "d4cb6a76120b5c73d3243a9e3222aa86",
+    "url": "tith.ICO"
+  },
+  {
+    "revision": "90e3e73822405f0ee8eeea27756b1541",
+    "url": "static/weibo-90e3e.min.png"
+  },
+  {
+    "revision": "bf762b85d6bc5feafb94fc973a575376",
+    "url": "static/head-bf762.min.jpg"
+  },
+  {
+    "revision": "01ad9fd832496a611cdea3fb170498a9",
+    "url": "static/demo4-01ad9.min.jpeg"
+  },
+  {
+    "revision": "9f33f70e0fadd0d2432114d9016e7618",
+    "url": "static/demo2-9f33f.min.png"
+  },
+  {
+    "revision": "8b1bc2bba6981dd6c8b38ff15ae23172",
+    "url": "static/demo1-8b1bc.min.png"
+  },
+  {
+    "revision": "15c9d061116dc4774fbd4285dcdb56e2",
+    "url": "static/bcg2-15c9d.min.jpg"
+  },
+  {
+    "revision": "edf88444910a4048020f1b7a4fba50e8",
+    "url": "static/bcg-edf88.min.jpg"
+  },
+  {
+    "revision": "9ad99948783c44d43fd1",
+    "url": "page-6e327.bundle.js"
+  },
+  {
+    "revision": "af666623eccdf93a4884cf80c4a20899",
+    "url": "index.html"
+  }
+];
